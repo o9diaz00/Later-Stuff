6586b4e5a1d2ea71dc17ce2881c00cdbca5cd99e
o9diaz00 <diazotis@gmail.com>
2025-06-12 17:31:54 -0400

    Update stats.go
    Reflected to mimic my Python equivalent script

--- GoLang/bucket-check/cmd/stats.go
+++ GoLang/bucket-check/cmd/stats.go
@@ -4,12 +4,11 @@ import (
     "fmt"
     "context"
     "log"
-    //"os"
-    "sync"
+    "time"
     "github.com/aws/aws-sdk-go-v2/aws"
     "github.com/aws/aws-sdk-go-v2/config"
     "github.com/aws/aws-sdk-go-v2/service/s3"
-    //"github.com/aws/aws-sdk-go-v2/service/s3/types"
+    s3types "github.com/aws/aws-sdk-go-v2/service/s3/types"
     "github.com/spf13/cobra"
 )
 
@@ -22,9 +21,22 @@ var (
     endpoint string
 )
 
+var getStatsCmd = &cobra.Command {
+    Use:    "stats",
+    Short:  "Get the size of the bucket (or directory specified with prefix)",
+    Run:    get_bucket_stats,
+}
+
 type customResolver struct {
     endpoint string
     region string
+}
+
+type Result struct {
+    Bucket string
+    Prefix string
+    Objects []s3types.Object
+    Error error
 }
 
 func (r customResolver) ResolveEndpoint(service, region string) (aws.Endpoint, error) {
@@ -38,16 +50,10 @@ func (r customResolver) ResolveEndpoint(service, region string) (aws.Endpoint, e
 func s3Client(ctx context.Context) (*s3.Client) {
     cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(region), config.WithSharedConfigProfile(profile), config.WithEndpointResolver(customResolver{region: region, endpoint: endpoint}))
     if (err != nil) {
-        log.Fatal(err)
+        log.Fatal(err);
     }
 
     return s3.NewFromConfig(cfg);
-}
-
-var getStatsCmd = &cobra.Command {
-    Use:    "stats",
-    Short:  "Get the size of the bucket (or directory specified with prefix)",
-    Run:    getSizeAndCount,
 }
 
 func convertBytes(size float64) (string) {
@@ -58,68 +64,72 @@ func convertBytes(size float64) (string) {
         size /= 1024;
         n += 1;
     }
-    return fmt.Sprintf("%.2f%s", size, t[n])
+    return fmt.Sprintf("%.2f%s", size, t[n]);
 }
 
-func getSizeAndCount(cmd *cobra.Command, args []string) {
-    ctx := context.Background();
-    client := s3Client(ctx);
-
-    var totalSize int64;
-    var totalCount int;
-    var wg sync.WaitGroup;
-    var mu sync.Mutex;
-
-    pageCh := make(chan *s3.ListObjectsV2Output);
-
-    for i := 0; i<concurrency; i+=1 {
-        wg.Add(1);
-        go func() {
-            defer wg.Done();
-            for page := range pageCh {
-                var size int64;
-                var count = len(page.Contents);
-                for _, obj := range page.Contents {
-                    size += *obj.Size;
-                }
-                mu.Lock();
-                totalSize += size;
-                totalCount += count;
-                mu.Unlock();
-            }
-        }()
-    }
-
-    paginator := s3.NewListObjectsV2Paginator(client, &s3.ListObjectsV2Input{
+func getBucketData(client *s3.Client, bucket string, prefix string) ([]s3types.Object, error) {
+    var data []s3types.Object;
+    paginator := s3.NewListObjectsV2Paginator(client, &s3.ListObjectsV2Input {
         Bucket: aws.String(bucket),
         Prefix: aws.String(prefix),
-    })
+        })
 
     for paginator.HasMorePages() {
-        page, err := paginator.NextPage(ctx);
+        page, err := paginator.NextPage(context.Background());
         if (err != nil) {
-            log.Fatalf("Error getting S3 page: %v", err);
+            return nil, err;
         }
-        pageCh <- page
+        data = append(data, page.Contents...);
     }
 
-    close(pageCh);
-    wg.Wait();
-
-    fmt.Printf("[%v]\n - Total Size: %s\n - Object Count: %d\n", bucket, convertBytes(float64(totalSize)), totalCount);
+    return data, nil;
 }
 
+func getBucketCount(data []s3types.Object) (int) {
+    return len(data);
+}
 
-func getLast() {
-    fmt.Printf("This gets the last file uploaded to the bucket: %v", bucket);
+func getBucketSize(data []s3types.Object) (int64) {
+    var size int64;
+    for _, d := range data {
+        size += *d.Size;
+    }
+    return size;
+}
+
+func getBucketLast(data []s3types.Object) (string, time.Time) {
+    var latest time.Time;
+    var name string;
+    for _, d := range data {
+        if (d.LastModified.After(latest)) {
+            latest = *d.LastModified;
+            name = *d.Key;
+        }
+    }
+    return name, latest;
+}
+
+func get_bucket_stats(cmd *cobra.Command, args []string) {
+    ctx := context.Background();
+    client := s3Client(ctx);
+    data, err := getBucketData(client, bucket, prefix);
+    last_key, last_time := getBucketLast(data);
+    if (err != nil) {
+        log.Fatalf("%v", err);
+    }
+    fmt.Printf("[%v]\n", bucket);
+    fmt.Printf(" - Total Objects: %d\n", getBucketCount(data));
+    fmt.Printf(" - Total Size: %v\n", convertBytes(float64(getBucketSize(data))));
+    fmt.Printf(" - Last Modified Object: %s\n - Last Modified Time: %s\n", last_key, last_time);
 }
 
 func init() {
-    getStatsCmd.Flags().StringVarP(&bucket, "bucket", "b", "", "The bucket in which we want to check (required)")
-    getStatsCmd.Flags().StringVarP(&prefix, "path", "p", "", "The subdirectory within the bucket we want to check")
-    getStatsCmd.Flags().StringVarP(&region, "region", "r", "us-iad-8", "The region at which the bucket is located")
-    getStatsCmd.Flags().IntVarP(&concurrency, "concurrency", "c", 10, "Number of concurrent workers")
-    getStatsCmd.Flags().StringVarP(&profile, "profile", "P", "default", "The profile used in the configuration file to authenticate with the endpoint")
-    getStatsCmd.Flags().StringVarP(&endpoint, "endpoint", "", "", "The endpoint at which the bucket exists")
-    _ = getStatsCmd.MarkFlagRequired("bucket")
+    getStatsCmd.Flags().StringVarP(&bucket, "bucket", "b", "", "The bucket in which we want to check (required)");
+    getStatsCmd.Flags().StringVarP(&prefix, "path", "p", "", "The subdirectory within the bucket we want to check");
+    getStatsCmd.Flags().StringVarP(&region, "region", "r", "", "The region at which the bucket is located");
+    getStatsCmd.Flags().StringVarP(&profile, "profile", "P", "default", "The profile used in the configuration file to authenticate with the endpoint");
+    getStatsCmd.Flags().StringVarP(&endpoint, "endpoint", "", "", "The endpoint at which the bucket exists");
+    _ = getStatsCmd.MarkFlagRequired("bucket");
+    _ = getStatsCmd.MarkFlagRequired("region");
+    _ = getStatsCmd.MarkFlagRequired("endpoint");
 }
